caa1973b858bc7840618af6984d36d44ac65bfc8
Saurabh <saurabhmadaan@gmail.com>
2013-02-06 23:32:49 -0800

    added kmeans

---
+++ KMeans/src/TestDrive.java
@@ -0,0 +1,15 @@
+
+public class TestDrive {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "[12,24]cost:12";
+		String[] b = a.split("cost:");
+		System.out.println(b[1]);
+
+	}
+
+}
